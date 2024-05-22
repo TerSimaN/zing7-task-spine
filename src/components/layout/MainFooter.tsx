d4e15ee4d4ddb0ab639f55ca163d0ccf8d363deb
TerSimaN <mcsiman998@gmail.com>
2024-05-22 16:37:23 +0300

    Added Main Header and Footer

---
+++ src/components/layout/MainFooter.tsx
@@ -0,0 +1,102 @@
+import Link from "next/link"
+import LogoMark from "../ui/LogoMark"
+import LogoTypo from "../ui/LogoTypo"
+import FacebookLogo from "../ui/FacebookLogo"
+import TwitterLogo from "../ui/TwitterLogo"
+import InstagramLogo from "../ui/InstagramLogo"
+
+const menu_section_links = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '#' },
+    { name: 'Services', href: '#' },
+    { name: 'FAQ', href: '#' },
+    { name: 'Contact', href: '#' }
+];
+
+const services_section_links = [
+    { name: 'Web Design & Development', href: '#' },
+    { name: 'Branding', href: '#' },
+    { name: 'Digital Marketing', href: '#' },
+    { name: 'Content Creation', href: '#' },
+    { name: 'Technical Support', href: '#' }
+];
+
+const MainFooter = () => {
+    return (
+        <footer className="bg-spine-neutral-700">
+            <div className="container lg:px-0 px-6 lg:pt-20 pt-[3.75rem]">
+                <div className="flex lg:flex-row flex-col lg:space-x-[8.5rem]">
+                    <div className="flex flex-col max-lg:items-center space-y-7 max-w-[23rem]">
+                        <Link href={'/'} className="flex items-end gap-x-1 w-full max-w-[12.5rem]">
+                            <LogoMark className="fill-spine-pale-lime w-7 h-10" />
+                            <LogoTypo className="fill-white w-[5.35rem] h-8" />
+                        </Link>
+                        <p className="font-poppins font-bold text-web-body-3 text-spine-neutral-300 lg:text-left text-center">
+                            Spine is the ultimate boost for your online presence.
+                            We're here to supercharge your digital agency's journey.
+                        </p>
+                        <div className="flex space-x-5">
+                            <Link href={'https://www.facebook.com/'}>
+                                <FacebookLogo className="fill-white" />
+                            </Link>
+                            <Link href={'https://twitter.com/'}>
+                                <TwitterLogo className="fill-white" />
+                            </Link>
+                            <Link href={'https://www.instagram.com/'}>
+                                <InstagramLogo className="fill-white" />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="flex lg:flex-row flex-col max-lg:items-center lg:space-x-[3.75rem] xl:mt-0 mt-20 w-full">
+                        <div className="space-y-5 w-full max-w-[9.25rem]">
+                            <h1 className="font-poppins font-bold text-web-body-1 text-white">Menu</h1>
+                            <ul className="space-y-3 font-poppins font-bold text-web-body-2 text-spine-neutral-300">
+                                {menu_section_links.map((link, index) => (
+                                    <li key={index}>
+                                        <Link href={link.href}>
+                                            {link.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="space-y-5 md:mt-0 mt-[3.75rem] w-full max-w-[9.125rem]">
+                            <h1 className="font-poppins font-bold text-web-body-1 text-white">Services</h1>
+                            <ul className="space-y-2 font-poppins font-bold text-web-body-2 text-spine-neutral-300">
+                                {services_section_links.map((link, index) => (
+                                    <li key={index}>
+                                        <Link href={link.href}>
+                                            {link.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="space-y-5 md:mt-0 mt-[3.75rem] w-full max-w-[15.75rem]">
+                            <h1 className="font-poppins font-bold text-web-body-1 text-white lg:text-left text-center">
+                                Join our newsletter
+                            </h1>
+                            <form className="flex flex-col lg:items-start items-center space-y-5">
+                                <input
+                                    className="bg-transparent border-[0.5px] border-spine-neutral-100 placeholder:font-poppins placeholder:font-bold placeholder:text-body-2 placeholder:text-spine-neutral px-3 py-3 w-full"
+                                    type="email"
+                                    placeholder="Enter email address"
+                                />
+                                <button className="bg-spine-pale-lime rounded-xl font-poppins font-bold text-button-2 text-spine-neutral-700 px-6 py-3">
+                                    Get Start
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center border-t border-white py-10 sm:mt-20 lg:mt-24 mt-16">
+                    <span className="font-poppins font-bold text-web-body-2 text-white">
+                        @2024 Spine ALL RIGHTS REVERSED
+                    </span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default MainFooter
---
+++ src/components/layout/MainHeader.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react";
+import LogoMark from "../ui/LogoMark";
+import LogoTypo from "../ui/LogoTypo";
+
+const links = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '#' },
+    { name: 'Services', href: '#' },
+    { name: 'FAQ', href: '#' },
+    { name: 'Contact', href: '#' }
+]
+
+const MainHeader = () => {
+    const [selected, setSelected] = useState(0);
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className="container">
+            <nav className="mx-auto flex items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <Link href={'/'} className="flex items-end gap-x-1 -m-1.5 p-1.5">
+                        <LogoMark className="fill-spine-neutral-700" />
+                        <LogoTypo className="fill-spine-neutral-700" />
+                    </Link>
+                </div>
+                <div className="flex lg:hidden">
+                    <button className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5" type="button" onClick={() => setOpen(!open)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-9 h-9 rotate-180">
+                            <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+                <ul className="hidden lg:flex gap-x-6">
+                    {links.map((link, index) => (
+                        <li key={index} className={`flex flex-col items-center font-poppins font-bold text-web-body-2 mx-6`}>
+                            <Link href={link.href} className={`${selected === index ? `text-spine-neutral-700` : `text-spine-neutral-500`}`} onClick={() => setSelected(index)}>
+                                {link.name}
+                            </Link>
+                            <div className={`border-2 rounded-full ${selected === index ? `border-spine-pale-lime` : `border-transparent`} w-5`}></div>
+                        </li>
+                    ))}
+                </ul>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <button className="border border-spine-neutral-700 rounded-xl font-poppins font-bold text-button-2 text-spine-neutral-700 px-6 py-3">
+                        Sign Up
+                    </button>
+                </div>
+            </nav>
+            <div className={`lg:hidden ${open ? `` : `hidden`}`}>
+                <div className="fixed inset-0 z-10"></div>
+                <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm">
+                    <div className="flex items-center justify-between">
+                        <Link href={'/'} className="flex items-end gap-x-1 -m-1.5 p-1.5">
+                            <LogoMark className="fill-spine-neutral-700" />
+                            <LogoTypo className="fill-spine-neutral-700" />
+                        </Link>
+                        <button className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5" type="button" onClick={() => setOpen(!open)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-9 h-9">
+                                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <ul className="space-y-2 py-6">
+                                {links.map((link, index) => (
+                                    <li key={index}>
+                                        <Link href={link.href} className="block font-poppins font-bold text-web-body-2 -mx-3 px-3 py-2" onClick={() => setSelected(index)}>
+                                            {link.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="py-6">
+                                <button className="block border rounded-xl font-poppins font-bold text-button-2 text-spine-neutral-700 -mx-3 px-6 py-3">Sign Up</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default MainHeader
