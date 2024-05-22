58b4a2ab90173ce95caaed3cb5db6de302ba6533
TerSimaN <mcsiman998@gmail.com>
2024-05-22 23:22:51 +0300

    Added images

---
+++ public/assets/images/index.ts
@@ -0,0 +1,17 @@
+import digital_marketing from './Digital-Marketing.svg';
+import bwink_edu_single from './bwink-edu-single.svg';
+import swirly_vector from './swirly-vector.svg';
+import about_img_1 from './About-img-1.svg';
+import service_img_1 from './Service-img-1.svg';
+import service_img_2 from './Service-img-2.svg';
+import service_img_3 from './Service-img-3.svg';
+
+export {
+    digital_marketing,
+    bwink_edu_single,
+    swirly_vector,
+    about_img_1,
+    service_img_1,
+    service_img_2,
+    service_img_3
+}
