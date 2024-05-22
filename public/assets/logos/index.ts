dd9f6626a985bd2d0819398cefed7aef776477a9
TerSimaN <mcsiman998@gmail.com>
2024-05-22 23:23:11 +0300

    Added logos

---
+++ public/assets/logos/index.ts
@@ -0,0 +1,13 @@
+import logoipsum_1 from './Logoipsum-1.svg';
+import logoipsum_2 from './Logoipsum-2.svg';
+import logoipsum_3 from './Logoipsum-3.svg';
+import logoipsum_4 from './Logoipsum-4.svg';
+import logoipsum_5 from './Logoipsum-5.svg';
+
+export {
+    logoipsum_1,
+    logoipsum_2,
+    logoipsum_3,
+    logoipsum_4,
+    logoipsum_5
+}
