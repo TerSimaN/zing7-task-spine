8ef31a72bcd2e5cca0a6e8b894b321a5a4080365
TerSimaN <mcsiman998@gmail.com>
2024-05-23 18:07:54 +0300

    Added new icons

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,5 +1,11 @@
 import list_point from './list-point.svg';
+import faq_icon from './FAQ-icon.svg';
+import arrow_caret_up from './fi-rr-caret-up.svg';
+import arrow_caret_down from './fi-rr-caret-down.svg';
 
 export {
-    list_point
+    list_point,
+    faq_icon,
+    arrow_caret_up,
+    arrow_caret_down
 }
