69fcd971b1160b9eaffaf58c36f6c64af822519f
TerSimaN <mcsiman998@gmail.com>
2024-05-23 18:11:00 +0300

    Added FAQs and Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import About from "@/components/sections/About";
+import Contact from "@/components/sections/Contact";
+import FAQs from "@/components/sections/FAQs";
 import Header from "@/components/sections/Header";
 import Service from "@/components/sections/Service";
 import TrustedBrand from "@/components/sections/TrustedBrand";
@@ -10,6 +12,8 @@ export default function Home() {
       <TrustedBrand />
       <About />
       <Service />
+      <FAQs />
+      <Contact />
     </main>
   );
 }
