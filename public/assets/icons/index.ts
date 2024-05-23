131cfcac587389328e08641fa59a37100d81538d
TerSimaN <mcsiman998@gmail.com>
2024-05-23 22:18:28 +0300

    Refactored code, added new icon

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -2,10 +2,12 @@ import list_point from './list-point.svg';
 import faq_icon from './FAQ-icon.svg';
 import arrow_caret_up from './fi-rr-caret-up.svg';
 import arrow_caret_down from './fi-rr-caret-down.svg';
+import arrow_east from './arrow-east.svg';
 
 export {
     list_point,
     faq_icon,
     arrow_caret_up,
-    arrow_caret_down
+    arrow_caret_down,
+    arrow_east
 }
--- src/components/layout/ServiceCard.tsx
+++ src/components/layout/ServiceCard.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image"
+import { arrow_east } from "../../../public/assets/icons"
 
 const ServiceCard = (props: {
     imgSrc: string,
     imgAlt: string,
     header: string,
     text: string,
-    btnText: string
+    btnText: string,
+    isVisible: boolean
 }) => {
     return (
-        <div className="flex lg:flex-row flex-col items-center lg:space-x-[3.75rem] lg:space-y-0 space-y-10">
+        <div className={`${props.isVisible ? `` : `hidden`} flex lg:flex-row flex-col items-center lg:space-x-[3.75rem] lg:space-y-0 space-y-10 lg:min-w-[73.125rem]`}>
             <div className="bg-white rounded-[1.75rem] lg:w-[25.5rem] w-[21rem] lg:h-[25.5rem] h-[21rem]">
                 <Image
                     src={props.imgSrc}
@@ -21,9 +23,12 @@ const ServiceCard = (props: {
                     <h1 className="font-sf-pro font-bold lg:text-web-display-4 text-display-2 text-spine-neutral-800">{props.header}</h1>
                     <p className="font-poppins font-bold lg:text-web-body-2 text-body-1 text-spine-neutral-600">{props.text}</p>
                 </div>
-                <button className="bg-spine-neutral-800 rounded-xl space-x-2 font-poppins font-bold text-button-2 text-white px-6 py-3 w-32">
+                <button className="flex items-center justify-between bg-spine-neutral-800 rounded-xl space-x-2 font-poppins font-bold text-button-2 text-white px-6 py-3 w-32">
                     {props.btnText}
-                    {/* Add arrow icon */}
+                    <Image
+                        src={arrow_east}
+                        alt="arrow east"
+                    />
                 </button>
             </div>
         </div>
