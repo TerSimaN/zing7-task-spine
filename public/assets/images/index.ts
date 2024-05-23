08bc519900f7a0570cbbf9d1969afd8ad7ffb009
TerSimaN <mcsiman998@gmail.com>
2024-05-23 20:32:18 +0300

    Added new images

--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -1,17 +1,21 @@
 import digital_marketing from './Digital-Marketing.svg';
 import bwink_edu_single from './bwink-edu-single.svg';
 import swirly_vector from './swirly-vector.svg';
+import underline_06 from './underline-06.svg';
 import about_img_1 from './About-img-1.svg';
 import service_img_1 from './Service-img-1.svg';
 import service_img_2 from './Service-img-2.svg';
 import service_img_3 from './Service-img-3.svg';
+import services_bg_image from './services-bg-image.jpg';
 
 export {
     digital_marketing,
     bwink_edu_single,
     swirly_vector,
+    underline_06,
     about_img_1,
     service_img_1,
     service_img_2,
-    service_img_3
+    service_img_3,
+    services_bg_image
 }
