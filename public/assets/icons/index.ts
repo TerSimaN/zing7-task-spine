ccd81d0407e06863d67124bf770813b4b128425c
TerSimaN <mcsiman998@gmail.com>
2024-05-24 19:20:08 +0300

    Added new icon

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -3,11 +3,13 @@ import faq_icon from './FAQ-icon.svg';
 import arrow_caret_up from './fi-rr-caret-up.svg';
 import arrow_caret_down from './fi-rr-caret-down.svg';
 import arrow_east from './arrow-east.svg';
+import no_icon from './no-icon.svg';
 
 export {
     list_point,
     faq_icon,
     arrow_caret_up,
     arrow_caret_down,
-    arrow_east
+    arrow_east,
+    no_icon
 }
